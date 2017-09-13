f32d0f5749548888e818dfc327270e5dc66388db
Ghousiah <31919253+Ghousiah@users.noreply.github.com>
2017-09-13 17:42:51 +0530

    This is my first basic project
    Currency converter is basically a converter of the currencies of the world like US dollar, Euro, Japanese Yen, German Mark.

---
+++ CurrencyConverter/CurrencyConverter/Converter.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Converter.aspx.cs" Inherits="CurrencyConverter.Converter" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="https://code.jquery.com/jquery-3.1.0.js" ></script>
+        <script>
+            //function convert() {
+            //    var input_amount = $("#input_amount").val();
+            //    var input_currdrp = $('#input_currdrp :selected').text();
+            //    var output_currdrp = $('#output_currdrp :selected').text();
+            //    if (input_amount === "" || input_amount === 0) {
+            //        alert("Please enter value greater than 0!");
+            //        return false;
+            //    }
+            //    else if (input_currdrp === output_currdrp) {
+            //        alert("Input & Output Currency should not be same!");
+            //        return false;
+            //    }
+            //    else {
+            //        var data = "input_amount=" + input_amount + "&input_currdrp=" + input_currdrp + "&output_currdrp=" + output_currdrp;
+            //        $.ajax({
+            //            url: "Converter.aspx/Button1_Click",
+            //            data: data,
+            //            success: function (result) {
+            //                $("#output_amount").val(result);
+            //            }
+            //        });
+            //    }
+            //}
+            function checkDec(el) {
+                var ex = /^[0-9]+\.?[0-9]*$/;
+                if (ex.test(el.value) == false) {
+                    el.value = el.value.substring(0, el.value.length - 1);
+                }
+            }
+        </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:DropDownList ID="DropDownList1" runat="server">
+            <asp:ListItem Value="0">US dollar</asp:ListItem>
+            <asp:ListItem Value="1">Euro</asp:ListItem>
+            <asp:ListItem Value="2">Japanese Yen</asp:ListItem>
+            <asp:ListItem Value="3">German Mark</asp:ListItem>
+        </asp:DropDownList>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Convert" />
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <asp:DropDownList ID="DropDownList2" runat="server">
+            <asp:ListItem Value="0">US dollar</asp:ListItem>
+            <asp:ListItem Value="1">Euro</asp:ListItem>
+            <asp:ListItem Value="2">Japanese Yen</asp:ListItem>
+            <asp:ListItem Value="3">German Mark</asp:ListItem>
+        </asp:DropDownList>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ CurrencyConverter/CurrencyConverter/Converter.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CurrencyConverter
+{
+    public partial class Converter : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "")
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Please enter value greater than 0!')", true);
+            }
+            else
+            {
+                if (System.Text.RegularExpressions.Regex.IsMatch(TextBox1.Text,
+                                                  "^[0-9]+\\.?[0-9]*$"))
+                {
+                    if (DropDownList1.Text == "0")
+                    {
+                        if (DropDownList2.Text == "1")
+                        {
+                            TextBox2.Text = (0.96 * Convert.ToDouble(TextBox1.Text)).ToString();
+                        }
+                        if (DropDownList2.Text == "2")
+                        {
+                            TextBox2.Text = (117.92 * Convert.ToDouble(TextBox1.Text)).ToString();
+                        }
+                        if (DropDownList2.Text == "3")
+                        {
+                            TextBox2.Text = (1.876 * Convert.ToDouble(TextBox1.Text)).ToString();
+                        }
+                    }
+
+                    if (DropDownList1.Text == "1")
+                    {
+                        if (DropDownList2.Text == "0")
+                        {
+                            TextBox2.Text = (Convert.ToDouble(TextBox1.Text) / 0.96).ToString();
+                        }
+                        if (DropDownList2.Text == "2")
+                        {
+                            TextBox2.Text = (122.83 * Convert.ToDouble(TextBox1.Text)).ToString();
+                        }
+                        if (DropDownList2.Text == "3")
+                        {
+                            TextBox2.Text = (1.954 * Convert.ToDouble(TextBox1.Text)).ToString();
+                        }
+                    }
+
+                    if (DropDownList1.Text == "2")
+                    {
+                        if (DropDownList2.Text == "0")
+                        {
+                            TextBox2.Text = (Convert.ToDouble(TextBox1.Text) / 117.92).ToString();
+                        }
+                        if (DropDownList2.Text == "3")
+                        {
+                            TextBox2.Text = (117.92 * (Convert.ToDouble(TextBox1.Text) / 1.876)).ToString();
+                        }
+                        if (DropDownList2.Text == "1")
+                        {
+                            TextBox2.Text = (0.96 * (Convert.ToDouble(TextBox1.Text) / 122.83)).ToString();
+                        }
+                    }
+
+                    if (DropDownList1.Text == "3")
+                    {
+                        if (DropDownList2.Text == "0")
+                        {
+                            TextBox2.Text = (Convert.ToDouble(TextBox1.Text) / 1.876).ToString();
+                        }
+                        if (DropDownList2.Text == "2")
+                        {
+                            TextBox2.Text = (122.83 * Convert.ToDouble(TextBox1.Text) / 1.954).ToString();
+                        }
+                        if (DropDownList2.Text == "1")
+                        {
+                            TextBox2.Text = (Convert.ToDouble(TextBox1.Text) / 1.954).ToString();
+                        }
+                    }
+                }
+                else
+                    Response.Write("Enter only numbers");
+            }
+            if (DropDownList1.Text == DropDownList2.Text)
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Input & Output Currency should not be same!')", true);
+            }
+        }
+    }
+}
